7e589cf0dc323b1c9ed1688a839bc21d252ef9fa
AyaAbdelrahman72 <75045153+AyaAbdelrahman72@users.noreply.github.com>
2020-12-06 00:49:03 +0200

    Login

---
+++ Doctor Panel.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Clinic_Managemt
+{
+    partial class Doctor_Panel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Doctor_Panel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Doctor_Panel";
+            this.Text = "Doctor_Panel";
+            this.Load += new System.EventHandler(this.Doctor_Panel_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Doctor Panel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Managemt
+{
+    public partial class Doctor_Panel : Form
+    {
+        
+        public Doctor_Panel()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Doctor_Panel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
